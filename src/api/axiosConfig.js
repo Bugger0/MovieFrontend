7a7afc01dc00fcc85d36f4af9e1bb0d113269a44
MOKASHI Zahirkhan (CONTRACTOR) <ZMI4@dsone.3ds.com>
2024-11-03 16:29:58 +0530

    axios updated

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://localhost:8080/',
+    baseURL:'https://movieserver-production-d9ad.up.railway.app/',
     headers: {"ngrok-skip-browser-warning": "true"}
 })
